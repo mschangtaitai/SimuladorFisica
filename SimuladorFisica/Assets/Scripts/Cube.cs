96f161c08b97c3227d2993b7654e8f2f0313f931
Michael Steven Chan Gonzalez <cha18562@uvg.edu.gt>
2019-05-22 10:31:26 -0600

    Simulador

--- SimuladorFisica/Assets/Scripts/Cube.cs
+++ SimuladorFisica/Assets/Scripts/Cube.cs
@@ -9,28 +9,37 @@ public class Cube : MonoBehaviour
 {
     private Rigidbody rigid;
     private BoxCollider collider;
+    private Transform transform;
     public Slider slider_friction;
     public Slider slider_k;
     public Slider slider_x;
     public Text FrictionText;
     public Text KText;
     public Text XText;
+    public Text HText;
+    private float maxH = 0;
 
     void Start()
     {
         rigid = GetComponent<Rigidbody>();
         collider = GetComponent<BoxCollider>();
+        transform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        collider.material.dynamicFriction = ((float)(0.1*slider_friction.value));
+        if(transform.position.y > maxH)
+        {
+            maxH = transform.position.y;
+            HText.text = maxH.ToString();
+        }
+        collider.material.dynamicFriction = ((float)(slider_friction.value));
         FrictionText.text = collider.material.dynamicFriction.ToString();
         bool Jump = Input.GetButtonDown("Jump");
-        float Force = (slider_k.value * 10) * (slider_x.value * 10);
-        KText.text = (slider_k.value * 10).ToString();
-        XText.text = (slider_x.value * 10).ToString();
+        float Force = (float)((slider_k.value * 1000) * (slider_x.value * 0.1));
+        KText.text = (slider_k.value * 1000).ToString();
+        XText.text = (slider_x.value * 0.1).ToString();
 
         if (Jump)
         {
